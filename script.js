89f84b1727998dac716c0cf05a6f68ccc2566ff7
Wagner G. Silva <wagsilgms@gmail.com>
2021-10-15 14:20:10 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let digital_h = document.querySelector('.digital .h');
+let digital_m = document.querySelector('.digital .m');
+let digital_s = document.querySelector('.digital .s');
+
+let sElement = document.querySelector('.p_s');
+let mElement = document.querySelector('.p_m');
+let hElement = document.querySelector('.p_h');
+
+function updateClock() {
+	let now    = new Date();
+	let hour   = now.getHours();
+	let minute = now.getMinutes();
+	let second = now.getSeconds();
+
+	digital_h.innerHTML = `${fixZero(hour)}`;
+	digital_m.innerHTML = `${fixZero(minute)}`;
+	digital_s.innerHTML = `${fixZero(second)}`;
+
+	let sDeg = ((360 / 60) * second) - 90;
+	let mDeg = ((360 / 60) * minute) - 90;
+	let hDeg = ((360 / 12) * hour) - 90;
+
+	sElement.style.transform = `rotate(${sDeg}deg)`;
+	mElement.style.transform = `rotate(${mDeg}deg)`;
+	hElement.style.transform = `rotate(${hDeg}deg)`;
+}
+
+function fixZero(time) {
+	return time < 10 ? `0${time}` : time;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
